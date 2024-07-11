b95e150721b51ca4554bcc4ad4d00212ecf5759e
dhanarrizky <dhanarrizky23@gmail.com>
2024-07-11 15:11:36 +0700

    start to code again

---
+++ learnStoreProcedure/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace learnStoreProcedure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
