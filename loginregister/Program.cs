96e2765763fbe632c498282a63447a48cf6237e4
dhanarrizky <dhanarrizky23@gmail.com>
2024-07-16 14:56:02 +0700

    createing blazor UI

---
+++ loginregister/Program.cs
@@ -0,0 +1,32 @@
+using loginregister.Components;
+
+namespace loginregister;
+
+public class Program {
+    public static void Main(string[]args){
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+
+    }
+}
